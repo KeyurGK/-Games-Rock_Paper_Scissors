80a57cfed1436d77ee505e1cc6a49cc819b13653
KeyurGK <56074740+KeyurGK@users.noreply.github.com>
2023-02-06 16:43:37 +0530

    commit

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-const computerChoiceDisplay = document.getElemnetById('computer-choice');
-const userChoiceDisplay = document.getElementById('user-choice');
+const computerChoiceDisplay = document.getElementById('computer-choice');
+const userChoiceDisplay = document.getElementById('your-choice');
 const resultDisplay = document.getElementById('result');
 
 const possibleChoices = document.querySelectorAll('button');
-
+let userChoice;
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e)=>{
-    let userChoice = e.target.id
-    userChoiceDisplay.innerHTML = userChoice;}
-));
+     userChoice = e.target.id
+    userChoiceDisplay.innerHTML = userChoice;
+    generateComputerChoice();
+}));
+
+function generateComputerChoice(){
+    const randomNumber = Math.floor(Math.random() * 3);
+    console.log(randomNumber);
+}
