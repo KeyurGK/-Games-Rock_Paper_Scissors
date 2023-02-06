53a14df9a3f6eb1204af1da24c9b4486c1f35ce8
KeyurGK <56074740+KeyurGK@users.noreply.github.com>
2023-02-06 16:35:32 +0530

    commit

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-const computerChoice = document.getElemnetById('computer-choice');
-const userChoice = document.getElementById('user-choice');
+const computerChoiceDisplay = document.getElemnetById('computer-choice');
+const userChoiceDisplay = document.getElementById('user-choice');
 const resultDisplay = document.getElementById('result');
+
+const possibleChoices = document.querySelectorAll('button');
+
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e)=>{
+    let userChoice = e.target.id
+    userChoiceDisplay.innerHTML = userChoice;}
+));
