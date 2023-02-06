537ea2e22cc3db4d32ea1be7e3d6608e0ef757f5
KeyurGK <56074740+KeyurGK@users.noreply.github.com>
2023-02-06 15:48:07 +0530

    commit

---
+++ app.js
@@ -0,0 +1,3 @@
+const computerChoice = document.getElemnetById('computer-choice');
+const userChoice = document.getElementById('user-choice');
+const resultDisplay = document.getElementById('result');
